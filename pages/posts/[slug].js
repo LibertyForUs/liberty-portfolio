fd6e741829602cb79788361ccd45291e276e6831
Cort Fritz <cortfritz@Corts-MacBook-Pro.local>
2022-10-29 13:27:54 -0700

    try test image with imported component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -27,6 +27,7 @@ const components = {
   // useful for conditionally loading components for certain routes.
   // See the notes in README.md for more details.
   Head,
+  Image,
 };
 
 export default function PostPage({
