109498df386690b8b5d968e8a3c1556da7b5fe51
Cort Fritz <cortfritz@Corts-MacBook-Pro.local>
2022-12-04 15:10:49 -0800

    change title

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Liberty Fritz';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Game Crazy';
+    : 'Please consider admitting me to your school.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
