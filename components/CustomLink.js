157da49a1c45623e0c739e6ede05ad6d173d689a
Cort Fritz <cort@rheli.co>
2024-10-27 12:11:48 -0700

    update dependencies

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function CustomLink({ as, href, ...otherProps }) {
   return (
-    <>
-      <Link as={as} href={href}>
-        <a {...otherProps} />
-      </Link>
-    </>
+    <Link 
+      as={as} 
+      href={href} 
+      {...otherProps}
+    />
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -6,17 +6,16 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
       <div style={{ textAlign: 'center' }}>
         <Image
-            src="/images/liberty-logo.png"
-            alt="Liberty is maxxed out"
-            height={40}
-            width={40} 
-          />
-          </div>
+          src="/images/liberty-logo.png"
+          alt="Liberty is maxxed out"
+          height={40}
+          width={40} 
+        />
+      </div>
 
       <p className="text-2xl dark:text-white text-center">
-
-        <Link href="/">
-          <a>{name}</a>
+        <Link href="/" className="hover:text-blue-600 dark:hover:text-blue-300">
+          {name}
         </Link>
       </p>
     </header>
