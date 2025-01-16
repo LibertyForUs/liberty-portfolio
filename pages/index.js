9b3e2a8e4322ad1da8da5ce47f7b22122015006a
Cort Fritz <cort@rheli.co>
2025-01-15 16:24:49 -0800

    add thumbnail system

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { getPosts } from '../utils/mdx-utils';
 
 import Footer from '../components/Footer';
@@ -29,18 +30,33 @@ export default function Index({ posts, globalData }) {
                 href={`/posts/[slug]`}
                 className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4"
               >
-                {post.data.date && (
-                  <p className="uppercase mb-3 font-bold opacity-60">
-                    {post.data.date}
-                  </p>
-                )}
-                <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                {post.data.description && (
-                  <p className="mt-3 text-lg opacity-60">
-                    {post.data.description}
-                  </p>
-                )}
-                <ArrowIcon className="mt-4" />
+                <div className="flex gap-6">
+                  {post.data.thumbnail && (
+                    <div className="flex-shrink-0 w-32 h-32 relative">
+                      <Image
+                        src={post.data.thumbnail}
+                        alt={`Thumbnail for ${post.data.title}`}
+                        fill
+                        className="object-cover rounded-lg"
+                        sizes="(max-width: 768px) 100px, 128px"
+                      />
+                    </div>
+                  )}
+                  <div className="flex-grow">
+                    {post.data.date && (
+                      <p className="uppercase mb-3 font-bold opacity-60">
+                        {post.data.date}
+                      </p>
+                    )}
+                    <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
+                    {post.data.description && (
+                      <p className="mt-3 text-lg opacity-60">
+                        {post.data.description}
+                      </p>
+                    )}
+                    <ArrowIcon className="mt-4" />
+                  </div>
+                </div>
               </Link>
             </li>
           ))}
